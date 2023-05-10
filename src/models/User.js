dba69f2a729058ca4631487de7fe75755da8b79a
Pedro Viana <pedroviana.developer13@gmail.com>
2023-05-10 16:21:57 -0300

    feat:user model

---
+++ src/models/User.js
@@ -0,0 +1,19 @@
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define('User', {
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            primaryKey: true
+        },
+        displayName: DataTypes.STRING,
+        email: DataTypes.STRING,
+        password: DataTypes.STRING,
+        image: DataTypes.STRING
+    }, {
+        tableName: 'users',
+        underscored: true,
+        timestamps: false
+    })
+
+    return User;
+}
